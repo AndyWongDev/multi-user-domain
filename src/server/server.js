b6582741e543f80e8481de8d72a509ba25fa597d
Andy Wong <andywong.ny@gmail.com>
2019-11-03 15:18:45 -0500

    added endpoint for bundle.js file

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT = 3000;
 
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static('assets'));
 app.use(express.static('views'));
+
+app.use('/dist', express.static(path.join(__dirname, '../../dist')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/views/index.html'));
